273dd6d32263f6355048efc8c8d2a5bd9d64032e
iagoscippo <iagoscippo@gmail.com>
2020-11-26 22:13:04 -0300

    segundo desafio

---
+++ desafio2.js
@@ -0,0 +1,7 @@
+let numIngresado = prompt("a ver, ingresá un número: ");
+
+if (numIngresado >= 20 && numIngresado <= 50) {
+  alert("eeeeeesa, justo era lo que necesitaba");
+} else {
+    alert("no me diste lo que quiero");
+}
